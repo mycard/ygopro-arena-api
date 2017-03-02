d889ab1cae28db2e882031fe25a96355d3feee7a
ganjingcun <343747757@qq.com>
2017-03-02 21:42:49 +0800

    remove db config

--- routes/api.js
+++ routes/api.js
@@ -13,15 +13,8 @@ var sqlite3 = require('sqlite3').verbose();
 // and client options
 // note: all config is optional and the environment variables
 // will be read if the config is not present
-var config = {
-    user: 'mycard', //env var: PGUSER
-    database: 'ygopro', //env var: PGDATABASE
-    password: 'dn8aSm9yAJx23qWn', //env var: PGPASSWORD
-    host: 'postgres.mycard.moe', // Server hosting the postgres database
-    port: 5432, //env var: PGPORT
-    max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-}
+
+var config = require('../db.config')
 
 //this initializes a connection pool
 //it will keep idle connections open for a 30 seconds
